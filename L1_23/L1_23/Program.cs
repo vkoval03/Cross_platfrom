3a2afa5be1409d1f18778d7f968d372556cc8722
vkoval03 <152928523+vkoval03@users.noreply.github.com>
2023-12-25 16:11:20 -0700

    Add files via upload

---
+++ L1_23/L1_23/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace L1_23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputInfo = File.ReadLines("input.txt").First().Split(' ');
+            (int x, int k) = (Convert.ToInt32(inputInfo[0]) / 5, Convert.ToInt32(inputInfo[1]));
+            var outputFileInfo = new FileInfo("output.txt");
+            using (StreamWriter streamWriter = outputFileInfo.CreateText())
+            {
+                streamWriter.WriteLine( (x + k).Factorial() / (x.Factorial() * k.Factorial()) );
+            }
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static int Factorial(this int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Factorial is defined only for non-negative integers.");
+            }
+
+            int result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ L2_23/L2_23/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace L2_23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            int[] coins = lines[1].Split(' ').Select(num => Convert.ToInt32(num)).ToArray();
+            int[] sums = lines[3].Split(' ').Select(num => Convert.ToInt32(num)).ToArray();
+
+            int[] result = CalculateChangePossibility(coins, sums);
+            File.WriteAllText("output.txt", string.Join(" ", result));
+        }
+
+        static int[] CalculateChangePossibility(int[] coins, int[] sums)
+        {
+            int[] result = new int[sums.Length];
+
+            for (int i = 0; i < sums.Length; i++)
+            {
+                result[i] = CanChange(coins, sums[i]) ? 1 : 0;
+            }
+
+            return result;
+        }
+
+        static bool CanChange(int[] coins, int target)
+        {
+            bool[] dp = new bool[target + 1];
+            dp[0] = true;
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                for (int j = coins[i]; j <= target; j++)
+                {
+                    dp[j] |= dp[j - coins[i]];
+                }
+            }
+
+            return dp[target];
+        }
+    }
+}
