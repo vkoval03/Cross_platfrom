8fc6cf424444166a83b6dcfda297bccc1e483e41
vkoval03 <152928523+vkoval03@users.noreply.github.com>
2023-12-26 07:00:18 -0700

    Add files via upload

---
+++ L5_23/LabsLibrary/L1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LabsLibrary
+{
+    public class L1Manager
+    {
+        private readonly int _x;
+        private readonly int _k;
+
+        public L1Manager (int x, int k)
+        {
+            _x = x / 5;
+            _k = k;
+        }
+
+        public string Run()
+        {
+            return Convert.ToString(CalculateFactorial(_x + _k) / (CalculateFactorial(_x) * CalculateFactorial(_k)));
+        }
+
+        private int CalculateFactorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Factorial is defined only for non-negative integers.");
+            }
+
+            int result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
