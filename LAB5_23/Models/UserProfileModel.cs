748d8651fcfc80ff29ffbbe5325e0cc95ab80a40
vkoval03 <152928523+vkoval03@users.noreply.github.com>
2023-12-26 06:55:04 -0700

    Delete LAB5_23 directory

--- LAB5_23/Models/UserProfileModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab5.Models
-{
-	public class UserProfileModel
-	{
-		public string Email { get; set; }
-		public string UserName { get; set; }
-		public string EmailVerified { get; set; }
-	}
-}
